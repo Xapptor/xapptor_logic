fa0d91cfc3d7fc39853e35dcc02b2b2834efca51
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2021-08-03 00:25:25 -0500

    firebase_tasks created

---
+++ lib/firebase_tasks.dart
@@ -0,0 +1,91 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+duplicate_document({
+  required String document_id,
+  required String collection_id,
+  required int times,
+}) async {
+  CollectionReference<Map<String, dynamic>> collection_reference =
+      FirebaseFirestore.instance.collection(collection_id);
+
+  await FirebaseFirestore.instance
+      .collection(collection_id)
+      .doc(document_id)
+      .get()
+      .then((document_snapshot) {
+    if (document_snapshot.data() != null) {
+      for (var i = 0; i < times; i++) {
+        collection_reference.add(document_snapshot.data()!);
+      }
+    }
+  });
+}
+
+update_field_in_collection({
+  required String field,
+  required String value,
+  required String collection,
+}) async {
+  QuerySnapshot collection_snapshot =
+      await FirebaseFirestore.instance.collection(collection).get();
+
+  for (var document in collection_snapshot.docs) {
+    document.reference.update({field: value});
+  }
+}
+
+delete_field_in_collection({
+  required String field,
+  required String collection,
+}) async {
+  QuerySnapshot collection_snapshot =
+      await FirebaseFirestore.instance.collection(collection).get();
+
+  for (var document in collection_snapshot.docs) {
+    document.reference.update({field: FieldValue.delete()});
+  }
+}
+
+duplicate_item_in_array_field({
+  required String document_id,
+  required String collection_id,
+  required String field_key,
+  required int index,
+  required int times,
+}) async {
+  await FirebaseFirestore.instance
+      .collection(collection_id)
+      .doc(document_id)
+      .get()
+      .then((document_snapshot) {
+    Map<String, dynamic> original_field_value =
+        document_snapshot.data()![field_key][index];
+
+    List original_array = document_snapshot.data()![field_key];
+
+    for (var i = 0; i < times; i++) {
+      original_array.add(original_field_value);
+    }
+
+    document_snapshot.reference.update({field_key: original_array});
+  });
+}
+
+update_item_in_array_field({
+  required String document_id,
+  required String collection_id,
+  required String field_key,
+  required String field_value,
+  required int index,
+}) async {
+  await FirebaseFirestore.instance
+      .collection(collection_id)
+      .doc(document_id)
+      .get()
+      .then((document_snapshot) {
+    List original_array = document_snapshot.data()![field_key];
+    original_array[index] = field_value;
+
+    document_snapshot.reference.update({field_key: original_array});
+  });
+}
