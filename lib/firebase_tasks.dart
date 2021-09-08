0b8d23d5f97a546d5e2f3173c3495ebd4eda519e
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2021-09-08 17:44:13 -0500

    firebase tasks improved

--- lib/firebase_tasks.dart
+++ lib/firebase_tasks.dart
@@ -1,5 +1,88 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+int collection_counter = 0;
+
+print_collection_counter() {
+  print("collection_counter $collection_counter");
+}
+
+// UPDATE
+
+update_field_in_collection({
+  required String field,
+  required dynamic value,
+  required String collection,
+}) async {
+  QuerySnapshot collection_snapshot =
+      await FirebaseFirestore.instance.collection(collection).get();
+
+  for (var document in collection_snapshot.docs) {
+    document.reference.update({field: value});
+    collection_counter++;
+  }
+  print_collection_counter();
+}
+
+update_field_name_in_collection({
+  required String collection,
+  required String old_field,
+  required String new_field,
+}) async {
+  QuerySnapshot collection_snapshot =
+      await FirebaseFirestore.instance.collection(collection).get();
+
+  for (var document in collection_snapshot.docs) {
+    //if (document.id == "IW1ZHfPPRRY6L6U1VHsii66grzY2") {
+    update_field_name_in_document(
+      collection: collection,
+      document: document,
+      old_field: old_field,
+      new_field: new_field,
+    );
+    //}
+    collection_counter++;
+  }
+  print_collection_counter();
+}
+
+update_field_name_in_document({
+  required String collection,
+  DocumentSnapshot? document,
+  String? document_id,
+  required String old_field,
+  required String new_field,
+}) async {
+  if (document == null) {
+    document = await FirebaseFirestore.instance
+        .collection(collection)
+        .doc(document_id)
+        .get();
+  }
+  document.reference.update(
+      {old_field: FieldValue.delete(), new_field: document.get(old_field)});
+}
+
+update_item_in_array_field({
+  required String document_id,
+  required String collection_id,
+  required String field_key,
+  required dynamic field_value,
+  required int index,
+}) async {
+  await FirebaseFirestore.instance
+      .collection(collection_id)
+      .doc(document_id)
+      .get()
+      .then((document_snapshot) {
+    List original_array = document_snapshot.data()![field_key];
+    original_array[index] = field_value;
+
+    document_snapshot.reference.update({field_key: original_array});
+  });
+}
+
+// DUPLICATE
+
 duplicate_document({
   required String document_id,
   required String collection_id,
@@ -19,31 +102,6 @@ duplicate_document({
       }
     }
   });
-}
-
-update_field_in_collection({
-  required String field,
-  required dynamic value,
-  required String collection,
-}) async {
-  QuerySnapshot collection_snapshot =
-      await FirebaseFirestore.instance.collection(collection).get();
-
-  for (var document in collection_snapshot.docs) {
-    document.reference.update({field: value});
-  }
-}
-
-delete_field_in_collection({
-  required String field,
-  required String collection,
-}) async {
-  QuerySnapshot collection_snapshot =
-      await FirebaseFirestore.instance.collection(collection).get();
-
-  for (var document in collection_snapshot.docs) {
-    document.reference.update({field: FieldValue.delete()});
-  }
 }
 
 duplicate_item_in_array_field({
@@ -74,21 +132,34 @@ duplicate_item_in_array_field({
   });
 }
 
-update_item_in_array_field({
-  required String document_id,
-  required String collection_id,
-  required String field_key,
-  required dynamic field_value,
-  required int index,
-}) async {
-  await FirebaseFirestore.instance
-      .collection(collection_id)
-      .doc(document_id)
-      .get()
-      .then((document_snapshot) {
-    List original_array = document_snapshot.data()![field_key];
-    original_array[index] = field_value;
+// DELETE
 
-    document_snapshot.reference.update({field_key: original_array});
+delete_field_in_collection({
+  required String field,
+  required String collection,
+}) async {
+  QuerySnapshot collection_snapshot =
+      await FirebaseFirestore.instance.collection(collection).get();
+
+  for (var document in collection_snapshot.docs) {
+    document.reference.update({field: FieldValue.delete()});
+    collection_counter++;
+  }
+  print_collection_counter();
+}
+
+delete_corrupted_accounts() async {
+  await FirebaseFirestore.instance.collection("users").get().then((collection) {
+    collection.docs.forEach((user) {
+      var user_data = user.data();
+      if (user_data["gender"] == null &&
+          user_data["birthday"] == null &&
+          user_data["country"] == null) {
+        print("id: ${user.id} user_data $user_data");
+        user.reference.delete();
+      }
+      collection_counter++;
+    });
+    print_collection_counter();
   });
 }
