e00869275854abd32989bb57f5da2509d4261936
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2021-09-17 19:21:01 -0500

    check user admin improved

--- lib/check_if_user_is_admin.dart
+++ lib/check_if_user_is_admin.dart
@@ -3,5 +3,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 Future<bool> check_if_user_is_admin(user_id) async {
   DocumentSnapshot user =
       await FirebaseFirestore.instance.collection("users").doc(user_id).get();
-  return user.get("admin") != null && user.get("admin");
+
+  Map user_data = user.data() as Map;
+  if (user_data["admin"] != null) {
+    return user_data["admin"];
+  } else {
+    return false;
+  }
 }
