8d4c9a64e9aa0e1140cf8944ec68e123ca3262b5
JavierGarciaWizeline <javierjesusgarciacontreras@gmail.com>
2022-04-14 15:02:29 -0500

    change_color_by_priority added

---
+++ lib/change_color_by_priority.dart
@@ -0,0 +1,20 @@
+String change_color_by_priority(String text, TextPriority text_priority) {
+  String color_code = "35";
+
+  if (text_priority == TextPriority.medium) {
+    color_code = "32";
+  } else if (text_priority == TextPriority.high) {
+    color_code = "33";
+  } else if (text_priority == TextPriority.urgent) {
+    color_code = "31";
+  }
+
+  return "\x1B[${color_code}m${text}\x1B[0m";
+}
+
+enum TextPriority {
+  low,
+  medium,
+  high,
+  urgent,
+}
