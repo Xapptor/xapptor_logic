90444becca2bc76557a81ffcf667ce7763a9b5fb
JavierGarciaWizeline <javierjesusgarciacontreras@gmail.com>
2022-04-15 20:02:59 -0500

    get_range_of_dates added

---
+++ lib/get_range_of_dates.dart
@@ -0,0 +1,18 @@
+List<DateTime> get_range_of_dates(DateTime date_1, DateTime date_2) {
+  int dates_difference_in_days = date_2.difference(date_1).inDays;
+
+  List<DateTime> range_of_dates = [];
+
+  for (var i = 0; i <= dates_difference_in_days; i++) {
+    range_of_dates.add(
+      DateTime(
+        date_1.year,
+        date_1.month,
+        date_1.day + i,
+      ),
+    );
+  }
+  //print("range_of_dates");
+  //print(range_of_dates);
+  return range_of_dates;
+}
