9b18f8dc04d94d26666fe04cd131a719e0e11ab0
JavierGarciaWizeline <javierjesusgarciacontreras@gmail.com>
2022-05-13 14:28:13 -0500

    url_launcher improved

--- lib/url_launcher.dart
+++ lib/url_launcher.dart
@@ -2,12 +2,18 @@ import 'package:url_launcher/url_launcher.dart';
 
 // Launch URL with fallback.
 
-Future<void> launch_url(String url, String fallback_url) async {
+launch_url(String url, String fallback_url) async {
   try {
-    bool launched = await launchUrl(Uri.parse(url));
-
-    if (!launched) await launchUrl(Uri.parse(fallback_url));
+    if (!await launchUrl(Uri.parse(url))) {
+      await launchUrl(
+        Uri.parse(fallback_url),
+      );
+    }
+    ;
   } catch (e) {
-    await launchUrl(Uri.parse(fallback_url));
+    print(e);
+    await launchUrl(
+      Uri.parse(fallback_url),
+    );
   }
 }
