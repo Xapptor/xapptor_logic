3483c2e3c47db6988854a990e0869ea55d2d6024
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2022-10-29 20:58:27 -0500

    is_portrait, get_asssets_names, url_launcher function path updated to xapptor_ui

--- lib/is_portrait.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Check if is portrait mode.
-
-bool is_portrait(BuildContext context) {
-  return MediaQuery.of(context).orientation == Orientation.portrait;
-}
