4398eaafb61c7599be37855e528cdd572f0b2ea7
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2022-12-15 01:04:44 -0600

    check_if_payments_are_enabled improved

--- lib/check_if_payments_are_enabled.dart
+++ lib/check_if_payments_are_enabled.dart
@@ -9,15 +9,20 @@ Future<bool> check_if_payments_are_enabled() async {
       .collection("metadata")
       .doc("payments")
       .get();
-  Map payment_data = payment_snap.data()!;
-  bool payment_enabled = false;
 
-  if (UniversalPlatform.isAndroid) {
-    payment_enabled = payment_data["enabled"]["android"] ?? false;
-  } else if (UniversalPlatform.isIOS) {
-    payment_enabled = payment_data["enabled"]["ios"] ?? false;
-  } else if (UniversalPlatform.isWeb) {
-    payment_enabled = payment_data["enabled"]["web"] ?? false;
+  if (payment_snap.exists) {
+    Map payment_data = payment_snap.data() as Map;
+    bool payment_enabled = false;
+
+    if (UniversalPlatform.isAndroid) {
+      payment_enabled = payment_data["enabled"]["android"] ?? false;
+    } else if (UniversalPlatform.isIOS) {
+      payment_enabled = payment_data["enabled"]["ios"] ?? false;
+    } else if (UniversalPlatform.isWeb) {
+      payment_enabled = payment_data["enabled"]["web"] ?? false;
+    }
+    return payment_enabled;
+  } else {
+    return false;
   }
-  return payment_enabled;
 }
