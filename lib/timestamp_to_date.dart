bfe37588dd6093034b770334c4e1598321f271f0
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2023-02-11 00:23:13 -0600

    timestamp_to_date_string updated

--- lib/timestamp_to_date.dart
+++ lib/timestamp_to_date.dart
@@ -3,7 +3,7 @@ import 'package:intl/intl.dart';
 
 // Convert timestamp to date.
 
-String timestamp_to_date(Timestamp time_stamp) {
+String timestamp_to_date_string(Timestamp time_stamp) {
   DateTime date_now = time_stamp.toDate();
   DateFormat date_formatter = DateFormat.yMMMMd('en_US');
   String date_now_formatted = date_formatter.format(date_now);
