1458ba17c5ab682dacf044353d8fe2571464fc0c
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2023-12-06 17:31:49 -0600

    Feat: file_downloader_web improved for large files

--- lib/file_downloader/file_downloader_web.dart
+++ lib/file_downloader/file_downloader_web.dart
@@ -1,28 +1,56 @@
 // ignore_for_file: avoid_web_libraries_in_flutter
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'dart:html';
-
-// Download file directly.
 
 class FileDownloader {
   FileDownloader._();
+
+  static FirebaseStorage storage = FirebaseStorage.instance;
+
+  static Future clean_temp_files() async {
+    ListResult temp_files = await storage.ref("temp").listAll();
+    for (Reference ref in temp_files.items) {
+      await ref.delete();
+    }
+  }
 
   static Future save({
     // Source could be Base64 or Url
     required String src,
     required String file_name,
   }) async {
+    await clean_temp_files();
     if (src.contains("http")) {
-      window.open(src, "_self");
+      _download(
+        src: src,
+        file_name: file_name,
+      );
     } else {
-      AnchorElement(
-        href: "data:application/octet-stream;charset=utf-16le;base64,$src",
-      )
-        ..setAttribute(
-          "download",
-          file_name,
-        )
-        ..click();
+      Reference temp_file = storage.ref("temp/$file_name");
+      await temp_file.putString(src);
+      String download_url = await temp_file.getDownloadURL();
+
+      _download(
+        src: download_url,
+        file_name: file_name,
+      );
     }
+  }
+
+  static Future _download({
+    required String src,
+    required String file_name,
+  }) async {
+    final anchor = AnchorElement(
+      href: src,
+    );
+    anchor.setAttribute(
+      "download",
+      file_name,
+    );
+    anchor.download = src;
+    anchor.click();
+    anchor.remove();
   }
 }
