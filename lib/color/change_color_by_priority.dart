564135f6f22e1420e0e8ce8a8546b342e92ccbad
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2024-07-03 11:50:53 -0600

    Feat: change_color_by_priority improved

--- lib/color/change_color_by_priority.dart
+++ lib/color/change_color_by_priority.dart
@@ -6,14 +6,16 @@ enum TextPriority {
 }
 
 String change_color_by_priority(String text, TextPriority text_priority) {
-  String color_code = "35";
-
-  if (text_priority == TextPriority.medium) {
-    color_code = "32";
-  } else if (text_priority == TextPriority.high) {
-    color_code = "33";
-  } else if (text_priority == TextPriority.urgent) {
-    color_code = "31";
+  String color_code = "";
+  switch (text_priority) {
+    case TextPriority.low:
+      color_code = "35";
+    case TextPriority.medium:
+      color_code = "32";
+    case TextPriority.high:
+      color_code = "33";
+    case TextPriority.urgent:
+      color_code = "31";
   }
   return "\x1B[${color_code}m$text\x1B[0m";
 }
--- lib/file_downloader/file_downloader.dart
+++ lib/file_downloader/file_downloader.dart
@@ -1,5 +1,3 @@
-// Export File Downloader dynamically for each platform.
-
 export 'file_downloader_unsupported.dart'
     if (dart.library.html) 'file_downloader_web.dart'
     if (dart.library.io) 'file_downloader_mobile.dart';
