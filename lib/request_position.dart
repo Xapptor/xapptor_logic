e670380e74b543c96f306cb30bad9591e36c3ead
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2024-09-18 23:20:00 -0600

    Feat: request_position improved

--- lib/request_position.dart
+++ lib/request_position.dart
@@ -1,41 +1,49 @@
-import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
-Future<Position> request_position() async {
+Future<Position?> request_position() async {
   bool serviceEnabled;
   LocationPermission permission;
 
   serviceEnabled = await Geolocator.isLocationServiceEnabled();
   if (!serviceEnabled) {
-    return Future.error('Location services are disabled.');
+    return null;
   }
 
   permission = await Geolocator.checkPermission();
   if (permission == LocationPermission.denied) {
     permission = await Geolocator.requestPermission();
     if (permission == LocationPermission.denied) {
-      return Future.error('Location permissions are denied');
+      return null;
     }
   }
 
   if (permission == LocationPermission.deniedForever) {
-    return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    return null;
   }
   return await Geolocator.getCurrentPosition();
 }
 
-Future<List<Placemark>> get_address_from_position(
+Future<List<Placemark>> get_address_from_position({
   double? lat,
   double? long,
-) async {
+}) async {
   if (lat == null || long == null) {
-    Position position = await request_position();
-    lat = position.latitude;
-    long = position.longitude;
+    Position? position = await request_position();
+
+    if (position != null) {
+      lat = position.latitude;
+      long = position.longitude;
+    }
   }
-  List<Placemark> placemarks = await placemarkFromCoordinates(
-    lat,
-    long,
-  );
+
+  List<Placemark> placemarks = [];
+
+  if (lat != null && long != null) {
+    placemarks = await placemarkFromCoordinates(
+      lat,
+      long,
+    );
+  }
   return placemarks;
 }
