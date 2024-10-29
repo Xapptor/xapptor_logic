86a26ccb6b8ae35de75c0bb71bbb7d83f1ab887a
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2024-10-29 15:09:55 -0600

    Feat: get_coupons_usage_info improved

--- lib/firebase_tasks/get.dart
+++ lib/firebase_tasks/get.dart
@@ -1,6 +1,7 @@
-import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_xlsio/xlsio.dart';
 import 'package:xapptor_logic/file_downloader/file_downloader.dart';
 import 'package:xapptor_logic/models/coupon.dart';
 import 'package:syncfusion_flutter_xlsio/xlsio.dart' as xlsio;
@@ -10,24 +11,57 @@ import 'package:xapptor_db/xapptor_db.dart';
 
 // Get coupons usage info.
 
-get_coupons_usage_info(
-  Timestamp date_created,
-) async {
-  QuerySnapshot coupon_query_snapshot =
-      await XapptorDB.instance.collection("coupons").where("date_created", isEqualTo: date_created).get();
+get_coupons_usage_info({
+  required Timestamp start_date,
+  Timestamp? end_date,
+  bool get_only_used_coupons = false,
+}) async {
+  debugPrint('get_coupons_usage_info');
 
-  List<DocumentSnapshot> coupon_query_snapshot_docs = coupon_query_snapshot.docs;
+  DateFormat long_formatter = DateFormat('yyyy/MM/dd HH:mm:ss');
+  DateFormat short_formatter = DateFormat('yyyy-MM-dd');
+
+  Query coupon_query = XapptorDB.instance.collection("coupons");
+
+  if (end_date == null) {
+    coupon_query = coupon_query.where(
+      "date_created",
+      isEqualTo: start_date,
+    );
+  } else {
+    coupon_query = coupon_query
+        .where(
+          "date_created",
+          isGreaterThanOrEqualTo: start_date,
+        )
+        .where(
+          "date_created",
+          isLessThanOrEqualTo: end_date,
+        );
+  }
+
+  if (get_only_used_coupons) {
+    coupon_query = coupon_query.where(
+      "user_id",
+      isNotEqualTo: "",
+    );
+  }
+
+  QuerySnapshot coupon_query_snapshot = await coupon_query.get();
+
+  List<DocumentSnapshot> coupon_query_docs = coupon_query_snapshot.docs;
 
   List<Coupon> coupons = [];
-  List<String> name_list = [];
+  List<String> names = [];
   List<String> course_was_completed_list = [];
+  List<String> certificate_dates = [];
 
-  coupon_query_snapshot_docs.removeWhere((element) => element.id == "template");
+  coupon_query_docs.removeWhere((element) => element.id == "template");
 
-  for (var element in coupon_query_snapshot_docs) {
+  for (var coupon_doc in coupon_query_docs) {
     Coupon coupon = Coupon.from_snapshot(
-      element.id,
-      element.data() as Map<String, dynamic>,
+      coupon_doc.id,
+      coupon_doc.data() as Map<String, dynamic>,
     );
 
     coupons.add(coupon);
@@ -41,8 +75,9 @@ get_coupons_usage_info(
 
       debugPrint("user_data -------------");
       debugPrint(fullname);
+      debugPrint('');
 
-      name_list.add(fullname);
+      names.add(fullname);
 
       bool course_was_completed = false;
 
@@ -51,41 +86,83 @@ get_coupons_usage_info(
       }
 
       course_was_completed_list.add(course_was_completed ? "Yes" : "No");
+
+      QuerySnapshot certificate_query =
+          await XapptorDB.instance.collection("certificates").where("user_id", isEqualTo: coupon.user_id).get();
+
+      if (certificate_query.docs.isNotEmpty) {
+        DocumentSnapshot certificate_snap = certificate_query.docs.first;
+
+        if (certificate_snap.data() != null) {
+          Map certificate_data = certificate_snap.data() as Map<String, dynamic>;
+          DateTime certificate_date = (certificate_data["date"] as Timestamp).toDate();
+
+          String certificate_date_string = long_formatter.format(certificate_date);
+
+          certificate_dates.add(certificate_date_string);
+        } else {
+          certificate_dates.add("");
+        }
+      } else {
+        certificate_dates.add("");
+      }
     } else {
-      name_list.add("");
+      names.add("");
       course_was_completed_list.add("");
     }
   }
 
-  Timer(const Duration(seconds: 3), () {
-    debugPrint(coupons.length.toString());
-    debugPrint(name_list.length.toString());
-    debugPrint(course_was_completed_list.length.toString());
+  final xlsio.Workbook workbook = xlsio.Workbook();
+  final xlsio.Worksheet sheet = workbook.worksheets[0];
 
-    final xlsio.Workbook workbook = xlsio.Workbook();
-    final xlsio.Worksheet sheet = workbook.worksheets[0];
+  sheet.getRangeByName("A1").setText("Coupon ID");
+  sheet.getRangeByName("B1").setText("Used by");
+  sheet.getRangeByName("C1").setText("The course was completed");
+  sheet.getRangeByName("D1").setText("Certificate Date");
+  sheet.getRangeByName("E1").setText("Total Pass Rate");
 
-    sheet.getRangeByName("A1").setText("Coupon ID");
-    sheet.getRangeByName("B1").setText("Used by");
-    sheet.getRangeByName("C1").setText("The course was completed");
+  final Style style = workbook.styles.add('Title');
+  style.fontSize = 12;
+  style.bold = true;
 
-    for (var i = 0; i < coupons.length; i++) {
-      sheet.getRangeByName("A${i + 2}").setText(coupons[i].id);
-      sheet.getRangeByName("B${i + 2}").setText(name_list[i]);
-      sheet.getRangeByName("C${i + 2}").setText(course_was_completed_list[i]);
-    }
+  sheet.getRangeByName("A1").cellStyle = style;
+  sheet.getRangeByName("B1").cellStyle = style;
+  sheet.getRangeByName("C1").cellStyle = style;
+  sheet.getRangeByName("D1").cellStyle = style;
+  sheet.getRangeByName("E1").cellStyle = style;
 
-    sheet.autoFitColumn(1);
-    sheet.autoFitColumn(2);
-    sheet.autoFitColumn(3);
+  for (var i = 0; i < coupons.length; i++) {
+    sheet.getRangeByName("A${i + 2}").setText(coupons[i].id);
+    sheet.getRangeByName("B${i + 2}").setText(names[i]);
+    sheet.getRangeByName("C${i + 2}").setText(course_was_completed_list[i]);
+    sheet.getRangeByName("D${i + 2}").setText(certificate_dates[i]);
+  }
+  int total_of_passes = course_was_completed_list.where((element) => element.toLowerCase() == "yes").length;
+  double total_pass_rate = total_of_passes * 100 / coupons.length;
 
-    String file_name = "coupons_usage_info_${date_created.toDate()}.xlsx";
-    file_name = file_name.replaceAll(":", "_").replaceAll("-", "_").replaceAll(" ", "_").replaceFirst(".", "_");
+  String total_pass_rate_string = "${total_pass_rate.toStringAsFixed(2)}%";
+  sheet.getRangeByName("E2").setText(total_pass_rate_string);
 
-    FileDownloader.save(
-      src: workbook.saveAsStream(),
-      file_name: file_name,
-    );
-    workbook.dispose();
-  });
+  sheet.autoFitColumn(1);
+  sheet.autoFitColumn(2);
+  sheet.autoFitColumn(3);
+  sheet.autoFitColumn(4);
+  sheet.autoFitColumn(5);
+
+  String start_date_string = short_formatter.format(start_date.toDate());
+  String end_date_string = end_date != null ? short_formatter.format(end_date.toDate()) : "";
+
+  String date_range = end_date != null ? "${start_date_string}_$end_date_string" : start_date_string;
+
+  String file_name = "coupons_usage_info_$date_range.xlsx";
+
+  debugPrint("Coupons: ${coupons.length}");
+  debugPrint("Pass Rate: $total_pass_rate_string");
+  debugPrint("File Name: $file_name");
+
+  FileDownloader.save(
+    src: workbook.saveAsStream(),
+    file_name: file_name,
+  );
+  workbook.dispose();
 }
